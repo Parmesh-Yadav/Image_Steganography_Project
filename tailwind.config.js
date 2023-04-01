6ca009a8232a12908f11a8952487e20b9a59dcb5
PARMESH YADAV <parmesh20319@iiitd.ac.in>
2023-04-01 06:36:43 +0530

    files added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        silkscreen: ["Silkscreen"],
+        sourcecodepro: ["Source Code Pro"],
+      },
+    },
+  },
+  plugins: [],
+}
