8d3f58c7d9f254c775ff1ffe4f0c5abbd8d8cc9d
PARMESH YADAV <parmesh20319@iiitd.ac.in>
2023-04-27 19:40:17 +0530

    project done

---
+++ dist/blob.js
@@ -0,0 +1,11 @@
+const blob = document.getElementById("blob");
+
+document.body.onpointermove = event => {
+    const {clientX,clientY} = event;
+    blob.animate({
+        left : `${clientX}px`,
+        top : `${clientY}px`
+    },{
+        duration:3000, fill:"forwards"
+    });
+}
